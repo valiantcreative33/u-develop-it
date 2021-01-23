9a47fef3c6d48b6ce58675f22df74a4d6dfe12e8
Ruben Matamoros <ruben@valiantcreative.net>
2021-01-23 15:05:31 -0500

    restructured and setup node.js application and sqlite3

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 // PORT designation and the app expression
 const PORT = process.env.PORT || 3001;
@@ -7,12 +8,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connects to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+      return console.error(err.message);
+    }
+
+    console.log('Connected to the election database.');
+});
+
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
     res.status(404).end();
 });
 
-// this starts the Express.js server on port 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
 });
